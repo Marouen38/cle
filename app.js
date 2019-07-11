a92fbd362305d795c6648d4f84d036519749c6a7
marouen38 <bachtobjimarouen@gmail.com>
2019-07-11 17:05:36 +0200

    V2

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('public'));
 app.use('/catalogue', catalogRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
