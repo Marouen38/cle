9106ee3e5c2d0100332cabd6da691032d67d6c1a
marouen38 <bachtobjimarouen@gmail.com>
2019-07-12 17:05:09 +0200

    MANU

--- api/catalogue.js
+++ api/catalogue.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 /* GET users listing. */
 router.get('/list', async(req, res, next)=> {
     try {
-        const smoothies = await smoothie.find({});
+        const smoothies = await Smoothie.find({});
         console.log('Liste de smoothie : ', smoothies);
         res.send(smoothies);
     }
@@ -33,5 +33,17 @@ router.get('/:id',async(req,res,next)=>{
     }
   
  })
+ router.post('/add', async (req, res, next) => {
+    ​
+        let newSmoothie = new Smoothie();
+        newSmoothie.title = req.body.title;
+    ​
+        try {
+            const smoothie = await newSmoothie.save();
+            res.send(smoothie);
+        } catch(err) {
+            res.status(400).send(err);
+        }
+    });
 
 module.exports = router;
